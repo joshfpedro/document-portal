5263d44de5f2c821dea9b6bd803047d1d23efa66
joshfpedro <33076791+joshfpedro@users.noreply.github.com>
2025-11-28 15:17:36 -0500

    feat: enhance DocumentPortalException to include detailed traceback information

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -7,10 +7,10 @@ logger = CustomLogger().get_logger(__file__)
 class DocumentPortalException(Exception):
     """Custom exception for Document Portal"""
     
-    def __init__(self, error_message, error_details:sys):
-        _,_,exc_tb = error_details.exc_info()
+    def __init__(self, error_message, error_details):
+        _, _, exc_tb = error_details.exc_info()
         exc_tb.tb_frame.f_code.co_filename
-        self.file_name=exc_tb.tb_frame.f_code.co_filename
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
         self.lineno = exc_tb.tb_lineno
         self.error_message = str(error_message)
         self.traceback_str = ''.join(traceback.format_exception(*error_details.exc_info()))
